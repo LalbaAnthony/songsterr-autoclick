f5d52faf1453f2091c1e79f924a30fabfe54a1f6
LalbaAnthony <97877743+LalbaAnthony@users.noreply.github.com>
2024-08-05 16:11:47 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,17 +1,13 @@
-document.addEventListener('DOMContentLoaded', function() {
-  // Function to repeatedly click the <a> element
-  function clickLink() {
-    // Select the <a> element inside the <p> with class 'rq1ph'
+// document.addEventListener('DOMContentLoaded', function() {
+  function closePopup() {
     var anchorElement = document.querySelector('.rq1ph a');
     if (anchorElement) {
-      // Trigger a click event on the <a> element
       anchorElement.click();
+      console.log('Popup closed.');
     }
 
-    // Call this function again after 100ms
     setTimeout(clickLink, 100);
   }
 
-  // Start the click loop
-  clickLink();
-});
+  closePopup();
+// });
