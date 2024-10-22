09807db550b04976a8624d75cec1e4fbe2c82c1f
LalbaAnthony <anthonylalaba24@gmail.com>
2024-10-22 19:13:16 +0200

    Update extension

--- src/main.js
+++ src/main.js
@@ -1,15 +1,24 @@
 
 function closePopup() {
-  var anchorElement = document.querySelector('.rq1ph a');
-  if (anchorElement) {
-    anchorElement.click();
-    console.log('Popup closed.');
-  } else {
-    console.log('Popup not found.');
-  }
+    var anchorElement = document.querySelector('.rq1ph a');
+    if (anchorElement) {
+        anchorElement.click();
+        console.log('Popup closed.');
+    } else {
+        console.log('Close button not found.');
+    }
 }
 
-document.getElementById('start-btn').addEventListener('click', () => {
-  setInterval(closePopup, 100);
-  console.log('Autoclick started.');
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Autoclick loaded.');
+    const startBtn = document.getElementById('start-btn');
+    if (startBtn) {
+        startBtn.addEventListener('click', () => {
+            setInterval(closePopup, 250);
+            console.log('Autoclick started.');
+        });
+        console.log('Start button found.');
+    } else {
+        console.log('Start button not found.');
+    }
 });
